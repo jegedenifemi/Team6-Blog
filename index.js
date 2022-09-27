c1e5cd0f83a696d2a4e915a37797ed147e4e56e3
jegedenifemi <jegedenifemi24@gmail.com>
2022-09-28 00:00:49 +0100

    updated links to other pages & started home page carousel

---
+++ index.js
@@ -0,0 +1,16 @@
+// Initialize Swiper (carousel)
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
