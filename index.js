3a527534c490dafe91aa44d7e13463fc0032aa43
jegedenifemi <jegedenifemi24@gmail.com>
2022-10-03 17:18:05 +0100

    mobile menu close button, nav dropdown, nav links hover state ...

--- index.js
+++ index.js
@@ -51,22 +51,31 @@ onUpdate();
 // MOBILE MENU
 let burgerBtn = document.querySelectorAll(".hamburger");
 let burgerMenu = document.querySelectorAll(".mobile_menu");
+let menuClose = document.querySelectorAll(".hamburger_close");
 
-// function showMenu(event) {
-//   console.log(22);
-//   burgerMenu[0].style.display = "block";
-// }
-
-burgerBtn[0].onclick = function showMenu() {
+burgerBtn[0].onclick = () => {
+  burgerBtn[0].style.display = "none";
+  menuClose[0].style.display = "block";
   document.querySelectorAll(".fa-magnifying-glass")[1].style.display = "none";
   document.querySelectorAll(".fa-magnifying-glass")[1].style.transition =
-    "30s ease-in-out";
+    "30s ease";
   burgerMenu[0].style.display = "block";
   burgerMenu[0].style.animation = "mobilemenushow .3s";
   burgerMenu[0].style.transition = ".5s ease";
 };
 
-// function showMenu(event) {
-//   console.log(22);
-//   burgerMenu[0].style.display = "block";
-// }
+menuClose[0].onclick = () => {
+  burgerBtn[0].style.display = "flex";
+  menuClose[0].style.display = "none";
+  document.querySelectorAll(".fa-magnifying-glass")[1].style.display = "block";
+  document.querySelectorAll(".fa-magnifying-glass")[1].style.transition =
+    "30s ease";
+  burgerMenu[0].style.display = "none";
+};
+
+if (window.innerWidth > 767) {
+  burgerMenu[0].style.display = "none";
+  burgerBtn[0].style.display = "none";
+  menuClose[0].style.display = "none";
+  document.querySelectorAll(".fa-magnifying-glass")[1].style.display = "none";
+}
