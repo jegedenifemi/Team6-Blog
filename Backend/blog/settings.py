cdf08739916c1402a880f67cb9ec9c4c02aa4cf6
John Tolulope Afariogun <afariogun.john@yahoo.com>
2022-10-13 20:39:41 +0100

    Update settings.py

--- Backend/blog/settings.py
+++ Backend/blog/settings.py
@@ -82,17 +82,17 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
-
-DATABASES ={
-    'default': dj_database_url.config()
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
+
+
+#DATABASES ={
+#    'default': dj_database_url.config()
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
