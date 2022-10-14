ef95701c0d08e2936ad5dcb45af6982542c7e6a7
johnafariogun <afariogun.john@yahoo.com>
2022-10-14 16:09:11 +0100

    histing

--- Backend/blog/settings.py
+++ Backend/blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from decouple import config 
 import dj_database_url
 from pathlib import Path
+import django_heroku
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # Web applications
     'accounts',
+    
     'post',
     # 3rd party applications
     'django_extensions',
@@ -153,3 +155,4 @@ EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = '465'
 
+django_heroku.settings(locals())
